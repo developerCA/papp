1a8eef64eefaa0e056b471e05a0a3a47e3f04c7d
johanna.calderon <johanna.calderon@xcelsa.com.ec>
2017-07-06 14:25:48 -0500

    Cambio en el corsfilter

--- pappWeb/src/ec/com/papp/web/comun/util/CorsFilter.java
+++ pappWeb/src/ec/com/papp/web/comun/util/CorsFilter.java
@@ -1,29 +1,34 @@
 package ec.com.papp.web.comun.util;
 
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import ec.com.xcelsa.utilitario.metodos.Log;
  
  
-public class CorsFilter extends OncePerRequestFilter {
-	private static Log log = new Log(CorsFilter.class);
-	 @Override
-	    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-	            throws ServletException, IOException {
-		 	System.out.println("entro al filtro");
-	        response.addHeader("Access-Control-Allow-Origin", "*");
-	        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-	        response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with, sid, mycustom, smuser");
-	        response.addHeader("Access-Control-Max-Age", "1800");//30 min
-	        filterChain.doFilter(request, response);
-	    }
-	}
+//public class CorsFilter extends OncePerRequestFilter {
+//	private static Log log = new Log(CorsFilter.class);
+//	 @Override
+//	    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//	            throws ServletException, IOException {
+//		 	System.out.println("entro al filtro");
+//	        response.addHeader("Access-Control-Allow-Origin", "*");
+//	        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//	        response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with, sid, mycustom, smuser");
+//	        response.addHeader("Access-Control-Max-Age", "1800");//30 min
+//	        filterChain.doFilter(request, response);
+//	    }
+//	}
 
 
+
+public class CorsFilter extends HttpServlet{
+
+	public void init(ServletConfig cfg,HttpServletResponse response) throws ServletException {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with, sid, mycustom, smuser");
+            response.addHeader("Access-Control-Max-Age", "1800");//30 min
+       // }
+    }
+}
