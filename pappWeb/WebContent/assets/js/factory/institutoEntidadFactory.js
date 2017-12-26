cffdde2a0ec89a9379960fc53406b69d739cddf4
elfiguero <31933897+elfiguero@users.noreply.github.com>
2017-12-25 19:09:32 -0500

    commit

--- pappWeb/WebContent/assets/js/factory/institutoEntidadFactory.js
+++ pappWeb/WebContent/assets/js/factory/institutoEntidadFactory.js
@@ -5,11 +5,11 @@ app.factory("institutoEntidadFactory", [ "Restangular", function(Restangular) {
 	return {
 
 		traerInstitutoEntidad : function(pagina) {
-			  return Restangular.allUrl("estructuraorganica/consultar/institutoentidad/pagina="+pagina+"&estado=A").getList();
+			  return Restangular.allUrl("estructuraorganica/consultar/institutoentidad/pagina="+pagina+"&estado=A&filas=25").getList();
 		},
 		
 		traerInstitutoEntidadFiltro : function(pagina,codigoInstitucion,nombreInstitucion,codigoEntidad,nombreEntidad,ejercicoFiscal,estado) {
-			var url = "estructuraorganica/consultar/institutoentidad/pagina="+pagina;
+			var url = "estructuraorganica/consultar/institutoentidad/pagina="+pagina+"&filas=25";
 
 			if(codigoInstitucion!=null && codigoInstitucion != "") url += "&codigoInstitucion=" + codigoInstitucion;	
 			if(nombreInstitucion!=null && nombreInstitucion != "") url += "&nombreInstitucion=" + nombreInstitucion;	
@@ -18,7 +18,7 @@ app.factory("institutoEntidadFactory", [ "Restangular", function(Restangular) {
 			if(ejercicoFiscal!=null && ejercicoFiscal != "") url += "&ejercicoFiscal=" + ejercicoFiscal;	
 			if(estado!=null && estado != "") url += "&estado=" + estado;
 
-console.log("FILTRO: "+url);
+			console.log("FILTRO: "+url);
 			return Restangular.allUrl(url).getList();
 		},
 
