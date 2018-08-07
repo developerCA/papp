b48479db5c2a29fe16d2688facbf71d437471077
ad <ad@DESKTOP-6ETUEQ3>
2018-08-07 10:04:17 -0500

    roles menu

--- pappWeb/WebContent/assets/js/directives/menu-to.js
+++ pappWeb/WebContent/assets/js/directives/menu-to.js
@@ -18,18 +18,17 @@ app.directive('navigation', ['$compile' ,'$sce','$animate', '_' , function($comp
             	 crearMenu();
               
           }, true);
-    	  
-    	  
-    	  
-    	           
+
           function crearMenu(){
-        	
         	  var menusPadre = _.filter(scope.menu, function(menu){ return menu.padreid==0 && menu.nombre.trim()!="" });
         	
         	  menuHtml=' <nav> <ul class="main-navigation-menu">';
         	  
         	  angular.forEach(menusPadre, function(men) {
-        		
+        		  if (!ifRollId(men.permisoid)) {
+        			  console.log("[RAIZ] NO MOSTRADO para:", men);
+        			  return;
+        		  }
         		  menuHtml+='<li ng-class="{\'active open\':$state.includes(\''+ '' +'\')}">';
          		  //menuHtml+='<li class="">';
         		  menuHtml+=' <a >';
@@ -53,46 +52,39 @@ app.directive('navigation', ['$compile' ,'$sce','$animate', '_' , function($comp
         	  });
         	  
         	  menuHtml+='</ul> </nav>';
-        	  
-        	  
+
         	  if (menuHtml!=""){
-        		        		  
             	  var content = $compile(menuHtml)(scope);
             	  elem.html(content);
-            	  
         	  }
           };
-         
-          
+
     	  function crearMenuHijos(hijos){
     	   var menuHtml="";
     	   
     	   menuHtml+='<ul class="sub-menu">';
     	   angular.forEach(hijos, function(men) {
-    		   
+     		  if (!ifRollId(men.permisoid)) {
+    			  console.log("NO MOSTRADO para:", men);
+    			  return;
+    		  }
     		   var menusHijos = _.filter(scope.menu, function(menu){ return menu.padreid==men.id && menu.nombre.trim()!="" });
-    	    	
-    		  
+
     		   if (menusHijos.length>0){
     			   menuHtml+='<li>';  
     			   menuHtml+='<a>';
     			   menuHtml+='<span>'+men.nombre+'</span><i class="icon-arrow"></i> </a>';
-    	    		  
     		   }else{
     			 menuHtml+='<li ui-sref-active="active">';
     			 if (men.ruta==""){
     				 menuHtml+='<a><span>'+men.nombre+'</span></a>';
-    	    			
     			 }else{
     				 menuHtml+='<a ui-sref="'+men.ruta+'"><span>'+men.nombre+'</span></a>';
-    	    				 
     			 }
     			 
       			 //menuHtml+='<a ui-sref="app.pagelayouts.fixedheader"><span>'+men.nombre+'</span></a>';
-    			 
       		   }
-    		  
-		    
+
     		   if (menusHijos.length>0){
      			  menuHtml+=  crearMenuHijos(menusHijos);
      		   }
--- pappWeb/WebContent/assets/js/seccion.rols.js
+++ pappWeb/WebContent/assets/js/seccion.rols.js
@@ -1,11 +1,29 @@
 // seccion
 seccion.rols = JSON.parse(seccion.rols);
-console.log(seccion.rols);
+seccion.rols = seccion.rols.permisos;
+//console.log(seccion.rols);
 
 function ifRollId(id) {
-	if (id === undefined) {
-
+	if (!isNaN(id)) {
+		id = parseInt(id);
+		if (id == 0) return true;
+		for (var obj in seccion.rols) {
+			if (seccion.rols[obj].id.permisoid == id) {
+				return true;
+			}
+		}
 	}
-	
+	return false;
+}
+
+function ifRollPermiso(nombre) {
+	if (nombre != undefined) {
+		if (nombre.trim() == "") return true;
+		for (var obj in seccion.rols) {
+			if (seccion.rols[obj].nppermiso == nombre) {
+				return true;
+			}
+		}
+	}
 	return false;
 }
