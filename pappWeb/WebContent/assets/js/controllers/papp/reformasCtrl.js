807bd64d316a64e5aba8a87a41002c90382d0ec4
Ivan A. Figueroa Jimenez <elfiguero@gmail.com>
2019-06-20 11:26:51 -0500

    excel

--- pappWeb/WebContent/assets/js/controllers/papp/reformasCtrl.js
+++ pappWeb/WebContent/assets/js/controllers/papp/reformasCtrl.js
@@ -345,7 +345,7 @@ app.controller('ReformasController', [ "$scope","$rootScope","$uibModal","SweetA
 			}
 		});
 		modalInstance.result.then(function(obj) {
-		    $scope.detalles = obj.reformalineas;
+		    $scope.detallesM = obj.reformalineas;
 		    $scope.detallesDP.push(false);
 			$scope.objeto.valorincremento = 0;
 			$scope.objeto.valordecremento = 0;
