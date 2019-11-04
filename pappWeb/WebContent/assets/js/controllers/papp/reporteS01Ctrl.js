6fd92b51c970a8a718b93690ce64f779bb143a43
Ivan Figueroa <elfiguero@gmail.com>
2019-11-03 23:43:40 -0500

    reportes Sxx

--- pappWeb/WebContent/assets/js/controllers/papp/reporteS01Ctrl.js
+++ pappWeb/WebContent/assets/js/controllers/papp/reporteS01Ctrl.js
@@ -75,21 +75,13 @@ app.controller('ReporteS01Controller', [ "$scope","$rootScope","$uibModal","Swee
 	$scope.unidadActivo = false;
 	$scope.abrirUnidad = function() {
 		var modalInstance = $uibModal.open({
-			templateUrl : 'assets/views/papp/modal/modalUnidadArbol.html',
-			controller : 'ModalUnidadArbolController',
-			size : 'lg',
-			resolve : {
-				instituicionFuente : function() {
-					return null; //$scope.objeto.institucionid;
-				},
-				institucionentidad : function() {
-					return $scope.objeto.entidadid;
-				}
-			}
+			templateUrl : 'assets/views/papp/modal/modalUnidad.html',
+			controller : 'ModalUnidadController',
+			size : 'lg'
 		});
 		modalInstance.result.then(function(obj) {
-			$scope.objeto.unidadid = obj.unidadarbolunidadid;
-			$scope.npunidad = obj.codigoorganico + ' - ' + obj.nombre;
+			$scope.objeto.unidadid = obj.id;
+			$scope.npunidad = obj.codigopresup + ' - ' + obj.nombre;
 			$scope.unidadActivo = true;
 			$scope.limpiarListas(3);
 		}, function() {
